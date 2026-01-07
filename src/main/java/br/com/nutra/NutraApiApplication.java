ae2834fc87fb79e51935194fcdeabd395cc596c6
GuiZimmermann <zimmerma83@gmail.com>
2026-01-07 18:14:51 -0400

    first commit

---
+++ src/main/java/br/com/nutra/NutraApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.nutra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nutra/NutraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nutra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
